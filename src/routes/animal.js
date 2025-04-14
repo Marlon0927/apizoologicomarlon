291332c8c12c95ab764b90c81d688f97b3d91860
Marlon0927 <marlonr.gomezskonradlorenz.edu.co>
2025-04-14 18:16:11 -0500

    finalizado, pendiente token

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const verifyToken = require('./validate_token');
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
 
 
 //Nuevo animal
-router.post("/animals", (req, res) => {
+router.post("/animals",(req, res) => {
     const animal = animalSchema(req.body);
     animal
         .save()
